6830d8fa1eeba88ddbb7243eec5002695a15c871
Zinchenko-Andrii <zinchenko.andrii@airslate.com>
2021-10-04 13:00:24 +0300

    t

--- .github/actions/release/dist/commonjs/index.js
+++ .github/actions/release/dist/commonjs/index.js
@@ -86281,8 +86281,6 @@ const gh = github_1(process.env.GITHUB_TOKEN);
 const args = { owner: owner.name || owner.login, repo: repository.name };
 
 (async function run() {
-  console.log('head_commit', context.payload.head_commit.message);
-
   const { message } = context.payload.head_commit;
 
   const branch = message.replace(message.slice(message.indexOf('\n\n')), '').split(`${process.env.ORGANIZATION}/`)[1];
@@ -86290,11 +86288,17 @@ const args = { owner: owner.name || owner.login, repo: repository.name };
   console.log('branch', branch);
   // console.log('commits', context.payload.commits[0].id);
   // console.log('context', context);
-  const tag = await gh.rest.git.getTag({ ...args, tag_sha: '023153772d82d3c64241aad9a8c9a33ea865c80d' });
-  // const tag = gh.rest.git.getTag({ ...args, tag_sha: context.payload.commits[0].id });
+  try {
+    const tag = await gh.rest.git.getTag({ ...args, tag_sha: '023153772d82d3c64241aad9a8c9a33ea865c80d' });
+    // const tag = gh.rest.git.getTag({ ...args, tag_sha: context.payload.commits[0].id });
+    console.log('tag', tag);
+
+  } catch (e) {
+    console.log(e);
+  }
+
 
   const tags = await gh.rest.repos.listTeams({ ...args });
 
-  console.log('tag', tag);
   console.log('tags', tags);
 }());
